5c4830a50b4f0101d7e2ebcb874c496202c45037
13911724083 <20925250@qq.com>
2026-07-25 02:47:06 +0800

    feat(web): register /login /signup /report/:id /predictions routes (T34)

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -5,6 +5,10 @@ import About from './pages/About';
 import Predict from './pages/Predict';
 import Pricing from './pages/Pricing';
 import NotFound from './pages/NotFound';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Report from './pages/Report';
+import Predictions from './pages/Predictions';
 
 export default function App() {
   return (
@@ -15,6 +19,10 @@ export default function App() {
           <Route path="/predict" element={<Predict />} />
           <Route path="/about" element={<About />} />
           <Route path="/pricing" element={<Pricing />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/report/:id" element={<Report />} />
+          <Route path="/predictions" element={<Predictions />} />
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
